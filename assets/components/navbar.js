094ced1c5ea2b1c52e597762f2483c1e2b254a12
izzulaverroes <146813701+izzulaverroes@users.noreply.github.com>
2024-02-26 17:22:41 +0700

    V 1.1

--- assets/components/navbar.js
+++ assets/components/navbar.js
@@ -1,33 +1,25 @@
 const V = document.title;
-let home = "";
-let about = "";
-let product = "";
-let news = "";
-let login = "";
-let cartVisibility = "";
-let logOut = "";
-let profile = "";
 
 if (V == "Bowo Bengkel") {
-  home = "#";
-  about = "about";
-  catalog = "catalog";
-  contact = "contact";
+    home = "#";
+    about = "about";
+    catalog = "catalog";
+    contact = "contact";
 } else if (V == "Bowo Bengkel | About") {
-  home = "../";
-  about = "#";
-  catalog = "../catalog";
-  contact = "../contact";
+    home = "../";
+    about = "#";
+    catalog = "../catalog";
+    contact = "../contact";
 } else if (V == "Bowo Bengkel | Catalog") {
-  home = "../";
-  about = "../about";
-  catalog = "#";
-  contact = "../contact";
+    home = "../";
+    about = "../about";
+    catalog = "#";
+    contact = "../contact";
 } else if (V == "Bowo Bengkel | Contact Us") {
-  home = "../";
-  about = "../about";
-  catalog = "../catalog";
-  contact = "#";
+    home = "../";
+    about = "../about";
+    catalog = "../catalog";
+    contact = "#";
 }
 
 const navbarBox = document.querySelector(".nav-");
@@ -35,14 +27,14 @@ let navbar = document.createElement("header");
 navbar.classList.add("fixed-top");
 navbar.innerHTML = `<nav class="navbar navbar-expand-lg position-fixed w-100 shadow-sm bg-primary fixed-top my-0 py-0 text-light" >
 <div class="container-md px-4 my-0 py-0">
-    <a class="navbar-brand" href="#home"><img src="assets/img/icon.png" alt="" class="w-50"></a>
+    <a class="navbar-brand d-flex" href="#home"><img src="assets/img/icon.png" alt="" class="w-25"><h2 class="text-white my-auto ms-3">Bowo Bengkel</h2></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
         aria-controls="offcanvasRight" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end my-0 py-0 bg-primary" id="offcanvasRight">
         <div class="offcanvas-header p-4 text-light">
-            <h3>Bowo Bengkel</h3>
+            <h2>Bowo Bengkel</h2>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="link ms-lg-auto me-4 me-lg-0 text-end text-light">
