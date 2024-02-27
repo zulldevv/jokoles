c70408f6f864840ff35413548a764eed92af7f1c
izzulaverroes <izzulaverroes@gmail.com>
2024-02-27 09:05:56 +0700

    V 1.5

--- assets/js/base.js
+++ assets/js/base.js
@@ -8,26 +8,6 @@ window.addEventListener('load', () => {
     })
 })
 
-// function setActiveLink() {
-//     var currentPath = document.title;
-
-//     // Remove "active" class from all links
-//     document.querySelectorAll('.nav-link').forEach(function (navLink) {
-//         navLink.classList.remove('nav-active');
-
-//         // Add "active" class to the link whose href matches the current page URL
-//         if (navLink.getAttribute('location') == currentPath) {
-//             navLink.classList.add('nav-active');
-//         }
-//     });
-// }
-
-// // Call the function on page load to set the initial active link
-// document.addEventListener('DOMContentLoaded', setActiveLink);
-
-// // Update the active link when the page URL changes
-// window.addEventListener('popstate', setActiveLink);
-
 function setActiveLink() {
     var currentTitle = document.title;
 
@@ -43,8 +23,8 @@ function setActiveLink() {
 }
 
 setActiveLink();
-// Call the function on page load to set the initial active link
-document.addEventListener('DOMContentLoaded', setActiveLink);
+// // Call the function on page load to set the initial active link
+// document.addEventListener('DOMContentLoaded', setActiveLink);
 
-// Update the active link when the document title changes
-document.addEventListener('DOMSubtreeModified', setActiveLink);
+// // Update the active link when the document title changes
+// document.addEventListener('DOMSubtreeModified', setActiveLink);
